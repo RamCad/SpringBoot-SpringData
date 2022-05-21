6c60b7b08fa7aeefc9b78ab0db3fa67ea19c3a61
Ramcharan <ramcharan.adapa@cdk.com>
2022-05-21 16:20:45 +0530

    Spring Boot with Spring Data JDBC template

---
+++ src/main/java/com/rc/spring/model/Book.java
@@ -0,0 +1,12 @@
+package com.rc.spring.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Book {
+  private int id;
+  private String name;
+  private String description;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into book(id, name, description) values(1,'Book1','Book1Desc');
+insert into book(id, name, description) values(2,'Book2','Book2Desc');
+insert into book(id, name, description) values(3,'Book3','Book3Desc');
